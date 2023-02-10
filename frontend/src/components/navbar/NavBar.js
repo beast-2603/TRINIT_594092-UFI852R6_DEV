88e90e7c6bbe87fbe6e498d9845871e08f56d2dc
Alok905 <alokranjanjoshi07567@gmail.com>
2023-02-11 03:33:23 +0530

    landing page created

---
+++ frontend/src/components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+import "./navbar.css";
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="nav_container">
+      <div className="nav_menu">
+        <ul>
+          <li className="nav_menu_item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav_menu_item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav_menu_item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav_menu_item">
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
